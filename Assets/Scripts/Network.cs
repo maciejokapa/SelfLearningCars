7acafdca45fd8dff7a1ebf81adcdb33ee455ee01
maciejokapa <34005915+maciejokapa@users.noreply.github.com>
2022-11-27 13:32:42 +0100

    Add files via upload

---
+++ Assets/Scripts/Network.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Network : MonoBehaviour {
+
+    public int generation;                      //numer generacji
+    public GameObject player;                   //prefab auta do postawienia
+    private PlayerController playerController;  //odwołanie do skryptu
+    private float[,] cars = new float[39,20];   //tablica zbiorcza do przechowywania w i b aktualnych samochodów
+    //Inicjalizacja
+    public void Start()
+    {
+        Init();
+    }
+
+    public int[] scores = new int[20];   //tablica do pzrechowywania fitnesow
+
+    private void Update()
+    {
+        CheckIfCarsAreDead();
+    }
+
+
+    // postawienie pierwszych 10 samochodów
+    public void Init () 
+    {
+        generation = 1;
+        for (int i = 0; i < cars.GetLength(1); i++)
+        {
+            GameObject car = Instantiate(player, Vector3.zero, Quaternion.identity);  //postawienie samochodu
+            playerController = car.GetComponent<PlayerController>();    //chwycenie odniesienia do skrytpu
+            playerController.ID = i;                                    //nadanie ID -> zapisywanie wyniku i znajdowanie najlepszych
+
+            for (int j = 0; j < cars.GetLength(0); j++)
+            {
+                cars[j, i] = Random.value * 2 - 1;                      //nadanie wartości w i b w tablicy zbiorczej
+
+                if (j <= 29)
+                    playerController.W[j] = cars[j, i];                 //nadanie wartości w w kontrolerze
+                else
+                    playerController.B[j - 30] = cars[j, i];            //nadanie wartości b w kontrolerze
+            }
+        }
+    }
+
+
+    float[] father = new float[39];   //tablice z w i b dwoch najlepszych
+    float[] mother = new float[39];
+    //jesli wszytskie samochody umarly znajdz najlepsze dwa w tablicy zbiorczej i stworz nowe pokolenie
+    public void CheckIfCarsAreDead ()
+    {
+        if (FindObjectsOfType<PlayerController>().Length == 0)  //jesli nie ma zadnego kontrolera na scenie
+        {
+            father = SearchForWinner();
+            mother = SearchForWinner();
+
+            CreateNewGen();
+            InstantiateNewGen();
+        }
+    }
+
+
+    //znajdz pozycje najlepszego w tablicy zbiorczej 
+    public float[] SearchForWinner ()
+    {
+        int bestScore = Mathf.Max(scores);     //zapamietanie najlepszego wyniku
+        for (int i = 0; i < scores.Length; i++)  
+        {
+            if (bestScore==scores[i])          //odnalezienie pozycji najleszego wyniku
+            {
+               scores[i] = 0;                    //wyzerowanie wyniku zeby znalezc za chwile drugiego rodzica
+               return  GetWinnerWeights(i);               
+            }
+        }
+        return new float[26];
+    }
+
+
+    //zwroc w i b najlepszego z tablicy zbiorczej
+    public float[] GetWinnerWeights (int i)
+    {
+        float[] parentWeights = new float[39];
+        for (int j = 0; j < cars.GetLength(0); j++)
+        {
+            parentWeights[j] = cars[j, i];
+        }
+        return parentWeights;
+    }
+
+
+    //wygeneruj nowe pokolenie na podstawie dwoch najlepszych
+    public void CreateNewGen ()
+    {
+        
+        for (int i = 0; i < cars.GetLength(0); i++)  //pierwsze dwa samochody takie jak najlepsze
+        {
+            cars[i, 0] = father[i];
+        }
+        for (int i = 0; i < cars.GetLength(0); i++)
+        {
+            cars[i, 1] = mother[i];
+        }
+        
+        CrossingOver();
+        Mutate();
+    }
+
+
+    //stworz dzieci jako mieszanka rodzicow
+    public void CrossingOver()
+    {             
+        for (int i = 2; i < cars.GetLength(1); i++)  //indeks 0 i 1 zajete przez rodzicow
+        {
+            for (int j = 0; j < cars.GetLength(0); j++)
+            {
+                float dice = Random.value;
+                if (dice>0.5f)                    //wartosc od jednego albo drugiego rodzica losowo
+                {
+                    cars[j, i] = father[j];
+                }
+                else
+                {
+                    cars[j, i] = mother[j];
+                }
+            }
+        }
+    }
+
+    //zmutuj wartosci dzieci
+    public  void Mutate ()      //indeks 0 i 1 zajete przez rodzicow a 2, 3, 4, 5 przez tych po crossingover bez mutacji
+    {          
+        for (int i = 6; i < 10; i++)  //w i b czterech dzieci zmienione o 10%
+        {
+            for (int j = 0; j < cars.GetLength(0); j++)
+            {
+                if (Random.value > 0.5f)
+                    cars[j, i] *= 1.1f;
+                else
+                    cars[j, i] *= -1.1f;
+            }
+        }
+        for (int i = 10; i < 14; i++)  //w i b czterech dzieci zmienione o 50%
+        {
+            for (int j = 0; j < cars.GetLength(0); j++)
+            {
+                if (Random.value > 0.5f)
+                    cars[j, i] *= 1.5f;
+                else
+                    cars[j, i] *= -1.5f;
+            }
+        }
+        for (int i = 14; i < cars.GetLength(1); i++)  //w i b pozostałych przypadkowe
+        {
+            for (int j = 0; j < cars.GetLength(0); j++)
+            {
+                
+                    cars[j, i] *= Random.Range(-5.0f,5.0f);
+            }
+        }
+    }
+
+
+    //postaw nastepne pokolenie
+    public void InstantiateNewGen ()
+    {
+        generation++;
+        for (int i = 0; i < cars.GetLength(1); i++)
+        {
+            GameObject car = Instantiate(player, Vector3.zero, Quaternion.identity);  //postawienie samochodu
+            playerController = car.GetComponent<PlayerController>();    //chwycenie odniesienia do skrytpu
+            playerController.ID = i;                                    //nadanie ID -> zapisywanie wyniku i znajdowanie najlepszych
+
+            for (int j = 0; j < cars.GetLength(0); j++)
+            {
+                if (j <= 29)
+                    playerController.W[j] = cars[j, i];                 //nadanie wartości w w kontrolerze
+                else
+                    playerController.B[j - 30] = cars[j, i];            //nadanie wartości b w kontrolerze
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Range(-1, 1)]
+    public float rotate;                       //output  (rotacja)
+    [Range(0, 1)]
+    public float speed;                        //output  (predkosc)
+
+    private float d1;                           //input  (odleglosci)
+    private float d2;                           //input
+    private float d3;                           //input
+
+
+    private Network network;    //odwolanie do skrytpu sieci
+    private void Start()
+    {
+        network = FindObjectOfType<Network>();
+    }
+
+
+    void Update()
+    {
+        Move();
+        Draw();
+        ComputeOutput();
+    }
+
+
+    public float[] W = new float[30];  //tablica wspolczynnikow
+    public float[] B = new float[9];   //tablica przesuniec
+    //obliczenie wartosci wyjsciowych na podstawie wspolczynikow
+    public void ComputeOutput ()
+    {       
+        float x1 = Tanh(Mathf.Clamp((d1 * W[0] + d2 * W[4] + d3 * W[8] + B[0]), -80, 80));
+        float x2 = Tanh(Mathf.Clamp((d1 * W[1] + d2 * W[5] + d3 * W[9] + B[1]), -80, 80));
+        float x3 = Tanh(Mathf.Clamp((d1 * W[2] + d2 * W[6] + d3 * W[10] + B[2]), -80, 80));
+        float x4 = Tanh(Mathf.Clamp((d1 * W[3] + d2 * W[7] + d3 * W[11] + B[3]), -80, 80));
+        float x5 = Tanh(Mathf.Clamp((x1 * W[12] + x2 * W[15] + x3 * W[18] + x4 * W[21] + B[4]), -80, 80));
+        float x6 = Tanh(Mathf.Clamp((x1 * W[13] + x2 * W[16] + x3 * W[19] + x4 * W[22] + B[5]), -80, 80));
+        float x7 = Tanh(Mathf.Clamp((x1 * W[14] + x2 * W[17] + x3 * W[20] + x4 * W[23] + B[6]), -80, 80));
+
+        float y1 = Tanh(Mathf.Clamp((x5 * W[24] + x6 * W[26] + x7 * W[28] + B[7]), -80, 80));
+        float y2 = Tanh(Mathf.Clamp((x5 * W[25] + x6 * W[27] + x7 * W[29] + B[8]), -80, 80));
+
+        speed = Sigm(Mathf.Clamp(y1, -80, 80));
+        rotate = Tanh(Mathf.Clamp(y2, -80, 80));    
+    }
+
+    
+    //funkcje aktywacyjne (tangens hiperboliczny i funkcja sigmoidalna)
+    public float Tanh (float x)
+    {
+        float y = (Mathf.Exp(x) - Mathf.Exp(-x)) / (Mathf.Exp(x) + Mathf.Exp(-x));
+        return y;
+    }
+    public float Sigm (float x)
+    {
+        float y = 1 / (1 + Mathf.Exp(-x));
+        return y;
+    }
+
+    public int ID;      //numer nadany przez skrypt Network  (wykrywanie najlepszych)
+    public int score;   //fitness score
+    //wykrywanie kolizji
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Wall"))  //smierć i przeslanie fitnesu do sieci
+        {
+            Destroy(gameObject);
+            network.scores[ID] = score;
+        }
+
+        if (collision.CompareTag("Checkpoint"))  //wzrost fitnesu
+        { 
+            score++;
+        }
+    }
+
+
+    public float rotationFactor = 50;  //wspolczynniki maksymalne skretu i predkosci
+    public float speedFactor = 20;
+    public float timeLimit = 60;
+    public float timeElapsed = 0;
+    //poruszanie samochodu na podstawie obliczonych wartosci predkosci i rotacji
+    public void Move()
+    {
+        transform.Rotate(0.0f, 0.0f, -rotate * rotationFactor * Time.deltaTime);
+
+        Vector3 localForward = transform.worldToLocalMatrix.MultiplyVector(transform.up);
+        Vector3 movement = localForward.normalized;
+        transform.Translate(movement * speed *speedFactor * Time.deltaTime);
+
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed>timeLimit)
+        {
+            Destroy(gameObject);
+            network.scores[ID] = score;
+        }
+    }
+
+
+    public Transform leftRayOrigin;    //pozycje do stworzenia "promienia"
+    public Transform leftRayDir;
+    public Transform frontRayOrigin;
+    public Transform frontRayDir;
+    public Transform rightRayOrigin;
+    public Transform rightRayDir;
+
+    public LayerMask whatIsWall;    //maska do wykrywania kolizji tylko ze scianami
+    //obliczanie wartosci wejsciowych (odleglosci) i rysowanie ich
+    public void Draw()
+    {
+        RaycastHit2D leftRay = Physics2D.Raycast(leftRayOrigin.position, leftRayDir.position - leftRayOrigin.position,200,whatIsWall);
+        if (leftRay.collider != null)
+        {
+            if (leftRay.collider.CompareTag("Wall"))
+            {
+                d1 = leftRay.distance;
+                Vector3 drawLeft = leftRayDir.position - leftRayOrigin.position;
+                drawLeft = drawLeft.normalized;
+                drawLeft = drawLeft * leftRay.distance;
+                Debug.DrawRay(leftRayOrigin.position, drawLeft, Color.yellow);
+                //Debug.Log(d1);
+            }
+        }
+
+        RaycastHit2D frontRay = Physics2D.Raycast(frontRayOrigin.position, frontRayDir.position - frontRayOrigin.position,200,whatIsWall);
+        if (frontRay.collider != null)
+        {
+            if (frontRay.collider.CompareTag("Wall"))
+            {
+                d2 = frontRay.distance;
+                Vector3 drawFront = frontRayDir.position - frontRayOrigin.position;
+                drawFront = drawFront.normalized;
+                drawFront = drawFront * frontRay.distance;
+                Debug.DrawRay(frontRayOrigin.position, drawFront, Color.yellow);
+                //Debug.Log(d2);
+            }
+        }
+
+        RaycastHit2D rightRay = Physics2D.Raycast(rightRayOrigin.position, rightRayDir.position - rightRayOrigin.position,200,whatIsWall);
+        if (rightRay.collider != null)
+        {
+            if (rightRay.collider.CompareTag("Wall"))
+            {
+                d3 = rightRay.distance;
+                Vector3 drawRight = rightRayDir.position - rightRayOrigin.position;
+                drawRight = drawRight.normalized;
+                drawRight = drawRight * rightRay.distance;
+                Debug.DrawRay(rightRayOrigin.position, drawRight, Color.yellow);
+                //Debug.Log(d3);
+            }
+        }
+    }
+}
